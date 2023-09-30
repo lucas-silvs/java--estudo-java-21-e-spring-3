a7a764d7a43fef2d1fe9c012a71150c9e3741baf
Lucas Santos <lucas.parkl@outlook.com>
2023-09-30 15:36:07 -0300

    Envio do projeto inicial

---
+++ estudo--spring-3-1-java-21/src/main/java/com/lucassilvs/estudospring31java21/EstudoSpring31Java21Application.java
@@ -0,0 +1,13 @@
+package com.lucassilvs.estudospring31java21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpring31Java21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpring31Java21Application.class, args);
+	}
+
+}
